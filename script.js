b930a56a30429fa500a235a5badccb0d54c65b12
Bastien Velcin <velcin.bastien@gmail.com>
2026-08-04 13:02:01 +0200

    Update script.js

--- script.js
+++ script.js
@@ -181,10 +181,10 @@ function renderMemoires() {
   `).join("");
 }
 
-/* function renderDiplomes() {
+function renderDiplomes() {
   const list = document.getElementById("diplomes-list");
   list.innerHTML = diplomes.map(d => `
-    <div class="memoire-item reveal in">
+    <div class="memoire-item reveal">
       <p class="year">${d.annee}</p>
       <div>
         <h3>${d.titre[currentLang]}</h3>
@@ -194,7 +194,7 @@ function renderMemoires() {
     </div>
   `).join("");
 }
-*/ 
+
 /* ===================================================================
    Langue : application au DOM + persistance
 =================================================================== */
@@ -293,6 +293,7 @@ document.addEventListener("DOMContentLoaded", () => {
   updateLangButtons();
   renderProjets();
   renderMemoires();
+   renderDiplomes();
   setupLangSwitch();
   setupScrollObservers();
   setupMobileMenu();
